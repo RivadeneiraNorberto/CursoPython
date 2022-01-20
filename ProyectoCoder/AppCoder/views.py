68aad802aae1d7ea2621987a1a0e5baa21cf321d
Norberto Rivadeneira <norberto.ariel@gmail.com>
2022-01-19 21:52:57 -0300

    agregado template boostrap

--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -1,11 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from AppCoder.models import Curso
+# from AppCoder.models import Curso
+
 
 # Create your views here.
-def curso(request):
-    curso = Curso(nombre="SQL Basico", camada=335984)
-    curso.save()
-    documentoTexto = f'Se guardó el Curso: {curso.nombre}  correspondiente a la Camada: {curso.camada}.'
+# def curso(request):
+#     curso = Curso(nombre="SQL Basico", camada=335984)
+#     curso.save()
+#     documentoTexto = f'Se guardó el Curso: {curso.nombre}  correspondiente a la Camada: {curso.camada}.'
     
-    return HttpResponse(documentoTexto)
+#     return HttpResponse(documentoTexto)
+
+def inicio(request):
+    return render(request, 'AppCoder/inicio.html')
+
+def cursos(request):
+    return render(request, 'AppCoder/cursos.html')
+
+def profesores(request):
+    return render(request, 'AppCoder/profesores.html')
+
+def estudiantes(request):
+    return render(request, 'AppCoder/estudiantes.html')
+
+def entregables(request):
+    return render(request, 'AppCoder/entregables.html')
